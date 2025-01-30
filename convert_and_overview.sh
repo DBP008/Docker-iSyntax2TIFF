3eda5832bca7812ab5507fe89149348decd593b2
DBP008 <69396814+DBP008@users.noreply.github.com>
2025-01-30 11:33:42 +0100

    Update convert_and_overview.sh to chmod and chwon to original permissions

--- convert_and_overview.sh
+++ convert_and_overview.sh
@@ -7,5 +7,8 @@ for file in *.isyntax
 do 
 	echo $file
 	python3 /root/PythonTools/isyntax_to_tiff.py "$file" 1 0 0 
-	python3 /root/PythonTools/dump_macro_label.py "$file" 
+	python3 /root/PythonTools/dump_macro_label.py "$file"
+ 	#chmod --reference=$file "${file%%.*}*" 
+  	chmod --reference=$file $(basename "$file").*
+   	chown --reference=$file $(basename "$file").*
 done

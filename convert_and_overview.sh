759c482a23bc9c845085515f1495366c9d29d191
DBP008 <69396814+DBP008@users.noreply.github.com>
2025-01-30 11:55:18 +0100

    Update convert_and_overview.sh

--- convert_and_overview.sh
+++ convert_and_overview.sh
@@ -8,7 +8,6 @@ do
 	echo $file
 	python3 /root/PythonTools/isyntax_to_tiff.py "$file" 1 0 0 
 	python3 /root/PythonTools/dump_macro_label.py "$file"
- 	#chmod --reference=$file "${file%%.*}*" 
-  	chmod --reference="$file" "${file%%.*}".*
-   	chown --reference="$file" "${file%%.*}".*
+  	chown --reference="$file" "${file%%.*}"*.*
+  	chmod --reference="$file" "${file%%.*}"*.*
 done
